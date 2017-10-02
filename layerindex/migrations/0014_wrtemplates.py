942ce88984f2d6ad98aa23feab93581c73f7f71a
Konrad Scherer <Konrad.Scherer@windriver.com>
2017-10-02 17:14:59 -0400

    layerindex/migrations: Add migrations for wrtemplates
    Auto-generated migration code from django.
    Update migration as part of the rebase.
    
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

---
+++ layerindex/migrations/0014_wrtemplates.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('layerindex', '0013_patch'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='WRTemplate',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.CharField(max_length=255)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('layerbranch', models.ForeignKey(to='layerindex.LayerBranch')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='WRTemplateFile',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=255)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('wrtemplate', models.ForeignKey(to='layerindex.WRTemplate')),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='layeritem',
+            name='layer_type',
+            field=models.CharField(choices=[('A', 'Base'), ('B', 'Machine (BSP)'), ('S', 'Software'), ('D', 'Distribution'), ('W', 'WRTemplates'), ('M', 'Miscellaneous')], max_length=1),
+        ),
+    ]
